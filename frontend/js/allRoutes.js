6bcd3d968fa0de5e706f855399151bf2f26eeb9b
oursMarmotte <jeangerald.centaure@gmail.com>
2024-10-17 21:01:36 +0200

    modification

--- frontend/js/allRoutes.js
+++
@@ -1,9 +0,0 @@
-import Route from "./Route.js";
-
-export const allRoutes= [
-    new Route("/","Accueil","/frontend/home.php"),
-    new Route("/galerie","La galerie","/frontend/galerie.php"),
-    new Route("/reserver","Réserver","/frontend/reserver.php"),
-];
-
-export const websitename ="Mon restaurant du coin";
